bd76adea5da6f1277d14995349646ecd0d0b2975
Kiran Hegde <kiransrivani@gmail.com>
2021-04-18 22:02:50 +0530

    made changes to UI and removed exit button

--- app.py
+++ app.py
@@ -32,6 +32,7 @@ def application():
     name = request.args['name']
     return render_template('application.html', name=name)
 
+
 @app.route('/test', methods=['POST'])
 def test():
 
@@ -47,6 +48,7 @@ def test():
     predicted = torch.max(output, 1)[1][0]
     return {'result': classes[predicted]}
 
+
 @app.route('/validate', methods=['POST'])
 def validate():
     file = request.files['image']
@@ -58,22 +60,24 @@ def validate():
 
     return {'loss': loss}
 
+
 @app.route('/sync', methods=['GET'])
 def sync():
     url = "http://localhost:8000/average"
 
-    payload={'name': request.args['name']}
-    files=[
-      ('model',('model.pt',open(weightsPath,'rb'),'application/octet-stream'))
+    payload = {'name': request.args['name']}
+    files = [
+        ('model', ('model.pt', open(weightsPath, 'rb'), 'application/octet-stream'))
     ]
 
     response = requests.request("POST", url, files=files, data=payload)
-    
+
     if(response.status_code == 200):
         print("Response received")
         open(weightsPath, 'wb').write(response.content)
 
     return ""
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/js/main.js
+++ static/js/main.js
@@ -38,6 +38,7 @@ $(document).ready(function() {
                     $("#testResultContainer").removeAttr("hidden");
                     $("#validationContainer").removeAttr("hidden");
                 }
+                
             })
         }
     });
